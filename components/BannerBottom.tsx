e0a011e7f8d24dc1d241baeae7ac547b5956f3a4
Okra <okra@Okras-MacBook-Pro.local>
2023-06-15 07:29:36 +0100

    colors and height for the banner

--- components/BannerBottom.tsx
+++ components/BannerBottom.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BannerBottom = () => {
   return (
-    <div className="max-w-7xl mx-auto flex justify-center items-center h-auto lg:h-60 bg-bgColor text-white py-10 px-8 -mt-10 z-50">
+    <div className="max-w-7xl mx-auto flex justify-center items-center h-auto lg:h-60 bg-bgColor text-primaryColors py-10 px-8 -mt-10 z-50">
       <div className="w-full lg:w-[90%] text-xl md:text-3xl lg:text-6xl text-center">
         WELCOME TO MY BLOG
       </div>
